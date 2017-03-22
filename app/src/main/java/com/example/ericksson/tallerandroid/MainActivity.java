fdee34dae86b21c6af3786f1b44ba737e4fcf8e1
ensalada <erickericksson94@gmail.com>
2017-03-22 13:46:52 -0300

    Segundo Commit
    Ejemplo tal como quedo al final de la clase 2

--- app/src/main/java/com/example/ericksson/tallerandroid/MainActivity.java
+++ app/src/main/java/com/example/ericksson/tallerandroid/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.ericksson.tallerandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind()
+
+    @OnClick(R.id.button_listo)
+    public void buttonListo(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //comentario
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
