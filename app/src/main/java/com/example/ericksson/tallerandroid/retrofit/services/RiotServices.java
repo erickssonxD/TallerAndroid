0e389ddc7ad22821c6de0d50e912fe7a39b70383
ensalada <erickericksson94@gmail.com>
2017-04-11 16:47:59 -0300

    Jeje
    Avaces

--- app/src/main/java/com/example/ericksson/tallerandroid/retrofit/services/RiotServices.java
+++ app/src/main/java/com/example/ericksson/tallerandroid/retrofit/services/RiotServices.java
@@ -9,13 +9,14 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface RiotServices {
 
     String API_BASE_URL = "https://api.github.com/";
 
-    @GET("/api/lol/{region}/v1.2/champion/{id}")
-    Call<ChampionInfo> infoForChampion(@Path("id") int id);
+    @GET("https://las.api.riotgames.com/api/lol/{region}/v1.2/champion/{id}")
+    Call<ChampionInfo> infoForChampion(@Path("region") String region, @Path("id") int id, @Query("api_key") String apiKey);
 
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(API_BASE_URL)
