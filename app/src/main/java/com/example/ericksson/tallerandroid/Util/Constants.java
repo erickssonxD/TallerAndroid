58a9e44eababc2c4b53ef79b3f4b51cd863982a5
ensalada <erickericksson94@gmail.com>
2017-04-14 14:19:09 -0300

    Bradcast y services

---
+++ app/src/main/java/com/example/ericksson/tallerandroid/Util/Constants.java
@@ -0,0 +1,9 @@
+package com.example.ericksson.tallerandroid.Util;
+//Created by ericksson on 22-04-27.
+
+
+public class Constants {
+    public static final String NUMERO_1 = "num1";
+    public static final String NUMERO_2 = "num2";
+    public static final String RESULTADO = "res";
+}
---
+++ app/src/main/java/com/example/ericksson/tallerandroid/Util/TallerActions.java
@@ -0,0 +1,8 @@
+package com.example.ericksson.tallerandroid.Util;
+//Created by ericksson on 11-04-17.
+
+
+public class TallerActions {
+    public static final String ACTION_MULTIPLY = "multipl";
+    public static final String ACTION_RECEIVE_RESULT = "result";
+}
